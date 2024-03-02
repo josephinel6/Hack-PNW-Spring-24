3822be58939d316b0fdc3d0346310a5f2a2d852b
XchraQeLymePi <69987770+XchraQeLymePi@users.noreply.github.com>
2024-03-02 10:57:11 -0800

    Add new feature

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, send_from_directory
+
+app = Flask(__name__)
+
+# Define the folder containing images
+IMAGE_FOLDER = 'images'
+
+# Route to serve images
+@app.route('/images/<path:filename>')
+def serve_image(filename):
+    return send_from_directory(IMAGE_FOLDER, filename)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+#blah blah balh test
