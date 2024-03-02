1f7129a04c6a526b5d9591d68d3e43406f16b029
Josephine <josephinemouse@gmail.com>
2024-03-02 14:51:46 -0800

    more style

--- app.py
+++ app.py
@@ -3,26 +3,7 @@ import os
 
 # setups and global variables
 app = Flask(__name__)
-socketio = SocketIO(app)
-
-UPLOAD_FOLDER = 'uploads'
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/')
 def home():
     return render_template('index.html')
-
-@app.route('/upload', methods=['POST'])
-def upload_files():
-    if 'folderUpload' not in request.files:
-        return 'No file part'
-    
-    files = request.files.getlist('folderUpload')
-    for file in files:
-        filename = file.filename
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-    
-    return 'Files uploaded successfully'
-
-if __name__ == '__main__':
-    socketio.run(app)
