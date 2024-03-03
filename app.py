06a1a74a7e65e22bcc7f6960f7cb6b59f73805b6
AnoushaMukherjee <123410504+AnoushaMukherjee@users.noreply.github.com>
2024-03-02 16:11:44 -0800

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,11 @@ import os
 
 # setups and global variables
 app = Flask(__name__)
+socketio = SocketIO(app)
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+if __name__ == '__main__':
+    socketio.run(app)
